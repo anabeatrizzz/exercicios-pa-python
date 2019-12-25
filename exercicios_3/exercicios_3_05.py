0f1334158140558e0d499be6a997fa2550ae1029
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-25 11:18:44 -0300

    Update exercicios_3_05.py

--- exercicios_3/exercicios_3_05.py
+++ exercicios_3/exercicios_3_05.py
@@ -1,66 +1,42 @@
-"""Escreva um programa que exiba o seguinte menu
-e crie um método para realizar os cálculos de cada item deste menu:
-1 – Soma
-2 – Subtração
-3 – Divisão
-4 – Multiplicação
-5 – Resto da Divisão
-6 – Dobro
-7 – Quadrado
-8 – Cubo
-9 – Raiz Quadrada
-0 – Sair"""
-
 import math
 
 def Operacoes(n, n1, n2):
-	conta = 0
 	if n == 1:
 		n1 = float(input("Digite um numero: "))
 		n2 = float(input("Digite outro numero: "))
-		print('A soma entre os numeros digitados é: ')
-		conta = n1 + n2
+		return f'A soma entre os numeros digitados é: {n1 + n2}'
 	elif n == 2:
 		n1 = float(input("Digite um numero: "))
 		n2 = float(input("Digite outro numero: "))
-		print(f'A subtração entre os numeros digitados é: ')
-		conta = n1 - n2
+		return f'A subtração entre os numeros digitados é: {n1 - n2}'
 	elif n == 3:
 		n1 = float(input("Digite um numero: "))
 		n2 = float(input("Digite outro numero: "))
-		print(f'A divisão entre os numeros digitados é: ')
-		conta = n1 / n2
+		return f'A divisão entre os numeros digitados é: {n1 / n2}'
 	elif n == 4:
 		n1 = float(input("Digite um numero: "))
 		n2 = float(input("Digite outro numero: "))
-		print(f'A multiplicação entre os numeros digitados é: ')
-		conta = n1 * n2
+		return f'A multiplicação entre os numeros digitados é: {n1 * n2}'
 	elif n == 5:
 		n1 = float(input("Digite um numero: "))
 		n2 = float(input("Digite outro numero: "))
-		print(f'O resto da divisão entre os numeros digitados é: ')
-		conta = n1 % n2
+		return f'O resto da divisão entre os numeros digitados é: {n1 % n2}'
 	elif n == 6:
 		n1 = float(input("Digite um numero: "))
 		n2 = 0
-		print(f'O dobro do numero digitado é: ')
-		conta = n1 * 2
+		return f'O dobro do numero digitado é: {n1 * 2}'
 	elif n == 7:
 		n1 = float(input("Digite um numero: "))
 		n2 = 0
-		print(f'O numero digitado ao quadrado é: ')
-		conta = n1 ** 2
+		return f'O numero digitado ao quadrado é: {n1 ** 2}'
 	elif n == 8:
 		n1 = float(input("Digite um numero: "))
 		n2 = 0
-		print(f"O numero digitado ao cubo é: ")
-		conta = n1 ** 3
+		return f'O numero digitado ao cubo é: {n1 ** 3}'
 	elif n == 9:
 		n1 = float(input("Digite um numero: "))
 		n2 = 0
-		print(f"A raiz quadrada do numero digitado é: ")
-		conta = math.sqrt(n1)
-	return conta
+		return f'A raiz quadrada do numero digitado é: {math.sqrt(n1)}'
 
 
 n1 = n2 = n3 = 0
