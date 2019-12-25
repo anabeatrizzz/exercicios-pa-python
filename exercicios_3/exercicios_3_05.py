b6344352fde226dc07f6346ac4771b1f83c667c6
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-25 11:19:54 -0300

    Update exercicios_3_05.py

--- exercicios_3/exercicios_3_05.py
+++ exercicios_3/exercicios_3_05.py
@@ -1,3 +1,16 @@
+"""Escreva um programa que exiba o seguinte menu
+e crie um método para realizar os cálculos de cada item deste menu:
+1 – Soma
+2 – Subtração
+3 – Divisão
+4 – Multiplicação
+5 – Resto da Divisão
+6 – Dobro
+7 – Quadrado
+8 – Cubo
+9 – Raiz Quadrada
+0 – Sair"""
+
 import math
 
 def Operacoes(n, n1, n2):
