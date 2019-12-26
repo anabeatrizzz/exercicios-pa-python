8f7d29c43b39a2d29eb02d7c07daf5bcb2a0d385
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-26 16:25:05 -0300

    Update exercicios_1_05.py

--- exercicios_1/exercicios_1_05.py
+++ exercicios_1/exercicios_1_05.py
@@ -1,4 +1,6 @@
-# Escreva um programa que leia a idade de uma pessoa e deverá ser exibido na tela, se esta pessoa é maior ou menor de idade (considerar 18 anos para maior idade). (Dica: Usar operadores ternários)
+# Escreva um programa que leia a idade de uma pessoa e deverá ser exibido na tela,
+# se esta pessoa é maior ou menor de idade (considerar 18 anos para maior idade).
+# (Dica: Usar operadores ternários)
 
 idade = int(input("Qual é sua idade? "))
 print('Você é', ("maior" if idade >= 18 else "menor"), 'de idade')
