77472a8ca0657418292068cd86fee24b57598226
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-26 16:35:27 -0300

    Update exercicios_2_10.py

--- exercicios_2/exercicios_2_10.py
+++ exercicios_2/exercicios_2_10.py
@@ -1,3 +1,12 @@
+"""Faça um programa que leia as variáveis C e N, respectivamente código e número de horas trabalhadas de um operário.
+Calcule o salário sabendo-se que ele ganha R$ 10,00 por hora.
+Quando o número de horas exceder a 50, calcule o excesso de pagamento armazenando-o na variável E,
+caso contrário zerar tal variável.
+A hora excedente de trabalho vale R$ 20,00.
+No final do processamento imprimir o salário total e o salário excedente.
+O programa só deve parar de rodar quando o usuário responder "S" na seguinte pergunta, "Deseja encerrar o programa?".
+"""
+
 E = 0
 C = int(input('Digite seu codigo: '))
 N = int(input('Numero de horas trabalhadas: '))
