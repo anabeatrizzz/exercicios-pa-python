98250e0d8d5689a044f57b71cda2d5abb7c9324f
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-26 16:31:32 -0300

    Update exercicios_2_07.py

--- exercicios_2/exercicios_2_07.py
+++ exercicios_2/exercicios_2_07.py
@@ -1,3 +1,6 @@
+"""Escreva um programa que exiba todos os números de 1 a 100 e a cada número que for múltiplo de 10,
+exiba a mensagem “MÚLTIPLO DE 10”."""
+
 for c in range(1, 100+1):
 	if c % 10 == 0:
 		print(f'{c} Múltiplo de 10')
