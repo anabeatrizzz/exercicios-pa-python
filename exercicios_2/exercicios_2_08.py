a7059da869dfb7ae8bbb190ba86dc725d8b03711
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-26 16:32:51 -0300

    Update exercicios_2_08.py

--- exercicios_2/exercicios_2_08.py
+++ exercicios_2/exercicios_2_08.py
@@ -1,3 +1,6 @@
+"""Escreva um programa que calcule o fatorial de um número informado pelo usuário.
+Dica: O fatorial de um número N é dado pela fórmula: N! = 1 * 2 * 3 * 4 * 5 * ... * N"""
+
 n = int(input('Digite um numero: '))
 c = n
 f = 1
