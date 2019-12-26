657428d527271997a2c594267ef8228035f5bde3
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-26 16:23:56 -0300

    Update exercicios_1_09.py

--- exercicios_1/exercicios_1_09.py
+++ exercicios_1/exercicios_1_09.py
@@ -1,4 +1,6 @@
-# Escreva um programa que leia dois números inteiros, sendo armazenados respectivamente nas variáveis A e B. O programa deverá inverter os valores entre as variáveis, de modo que o valor da variável A seja atribuído na variável B e vice-versa. Ao final exibir os valores de cada variável.
+# Escreva um programa que leia dois números inteiros, sendo armazenados respectivamente nas variáveis A e B.
+# O programa deverá inverter os valores entre as variáveis, de modo que o valor da variável A seja atribuído
+# na variável B e vice-versa. Ao final exibir os valores de cada variável.
 
 A = int(input("Digite um numero: "))
 B = int(input("Digite outro numero: "))
