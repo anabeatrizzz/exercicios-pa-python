d554d56bc0b16a93d105e4ccc7bfa29994904a34
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-26 16:30:10 -0300

    Update exercicios_2_06.py

--- exercicios_2/exercicios_2_06.py
+++ exercicios_2/exercicios_2_06.py
@@ -1,3 +1,7 @@
+"""Escreva um programa que calcule e exiba a média de 10 notas digitadas pelo usuário.
+Considerar notas válidas, somente valores entre 0 (zero) e 10 (dez).
+Se o usuário digitar algum valor inválido, deverá ser exibida uma mensagem informando o ocorrido."""
+
 nota = soma = media = 0
 for c in range(1, 10+1):
 	nota = float(input(f'Escreva a {c}a nota: '))
