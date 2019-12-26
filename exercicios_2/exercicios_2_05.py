36a029eb72068a409eb9874dc947b9b10960a8b9
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-26 16:29:26 -0300

    Update exercicios_2_05.py

--- exercicios_2/exercicios_2_05.py
+++ exercicios_2/exercicios_2_05.py
@@ -1,3 +1,5 @@
+"""Escreva um programa que leia 15 números inteiros e exiba na tela ao final, o maior número que foi digitado pelo usuário."""
+
 maior = 0
 for c in range(1, 15+1):
 	n = int(input('Digite um numero: '))
