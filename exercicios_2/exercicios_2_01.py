cea367898432fa05beebe86d2105a3590488b307
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-27 17:39:51 -0300

    Update exercicios_2_01.py

--- exercicios_2/exercicios_2_01.py
+++ exercicios_2/exercicios_2_01.py
@@ -1,7 +1,5 @@
 """Escreva um programa que exiba na tela em ordem crescente, apenas os números pares existentes de 11 a 250."""
 
-imp = 0
 for c in range(11, 250+1):
 	if c % 2 != 0:
-		imp = c
-		print(imp)
+		print(c)
