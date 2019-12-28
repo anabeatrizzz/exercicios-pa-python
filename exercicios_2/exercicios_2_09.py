d706edc5d2a170adb635db9d8473e3a2e6ff865a
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-28 19:52:11 -0300

    Update exercicios_2_09.py

--- exercicios_2/exercicios_2_09.py
+++ exercicios_2/exercicios_2_09.py
@@ -3,22 +3,8 @@ quantos números ímpares, quantos números positivos e quantos números negativ
 O programa só deve parar de rodar quando o usuário responder "S" na seguinte pergunta, "Deseja encerrar o programa?".
 """
 
-for c in range(1, 5+1):
-		n = int(input('Digite um numero: '))
-		if n % 2 == 0:
-			a += 1
-		if n % 2 != 0:
-			b += 1
-		if n > 0:
-			c += 1
-		if n < 0:
-			d += 1
-	print(f'{a} valor(es) par(es)')
-	print(f'{b} valor(es) impar(es)')
-	print(f'{c} valor(es) positivo(s)')
-	print(f'{d} valor(es) negativo(s)')
-resp = int(input('Deseja encerrar o programa?\n1 - Sim\n2- Nao'))
-while resp == 2:
+a = b = c = d = 0
+while True:
 	for c in range(1, 5+1):
 		n = int(input('Digite um numero: '))
 		if n % 2 == 0:
@@ -29,10 +15,10 @@ while resp == 2:
 			c += 1
 		if n < 0:
 			d += 1
-	print(f'{a} valor(es) par(es)')
+	print(f'\n{a} valor(es) par(es)')
 	print(f'{b} valor(es) impar(es)')
 	print(f'{c} valor(es) positivo(s)')
 	print(f'{d} valor(es) negativo(s)')
-resp = int(input('Deseja encerrar o programa?\n1 - Sim\n2- Nao'))
+	resp = int(input('\nDeseja encerrar o programa?\n1 - Sim\n2- Nao\n'))
 	if resp == 1:
 		break
