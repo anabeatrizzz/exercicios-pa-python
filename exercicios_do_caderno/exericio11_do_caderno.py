d7c921d3753a78d745e577073674824df3d63847
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-28 11:02:40 -0300

    Update exericio11_do_caderno.py

--- exercicios_do_caderno/exericio11_do_caderno.py
+++ exercicios_do_caderno/exericio11_do_caderno.py
@@ -1,4 +1,5 @@
-"""Elabore um programa que receba o nome e as 4 notas dos alunos, calcule a media e mostre na tela se o aluno foi aprovado com media maior ou igual a 6.
+"""Elabore um programa que receba o nome e as 4 notas dos alunos, calcule a media e mostre na tela
+se o aluno foi aprovado com media maior ou igual a 6.
 Após cada processo, pergunte se o usuario quer sair.
 Se sim, pare o programa, se não, volte a pedir os dados de mais um aluno."""
 
