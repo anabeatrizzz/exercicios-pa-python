520fee8de13123c8dc64e1a2bd1a6a24bf3975e2
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-28 19:53:32 -0300

    Update exercicios_3_03.py

--- exercicios_3/exercicios_3_03.py
+++ exercicios_3/exercicios_3_03.py
@@ -1,15 +1,10 @@
-"""Escreva um programa que leia três valores para os lados
-de um triângulo (variáveis A, B e C).
+"""Escreva um programa que leia três valores para os lados de um triângulo (variáveis A, B e C).
 Verificar se cada lado é menor que a soma dos outros dois lados.
-Se sim, saber de A==B e se B==C,
-sendo verdade o triângulo é equilátero;
-Se não, verificar de A==B ou se A==C ou se B==C,
-sendo verdade o triângulo é isósceles;
+Se sim, saber de A==B e se B==C, sendo verdade o triângulo é equilátero;
+Se não, verificar de A==B ou se A==C ou se B==C, sendo verdade o triângulo é isósceles;
 E caso contrário, o triângulo será escaleno.
-Caso os lados fornecidos não caracterizarem um triângulo,
-avisar a ocorrência.
-Deverá ser implementado um método para verificar a validade
-de cada tipo de triângulo.
+Caso os lados fornecidos não caracterizarem um triângulo, avisar a ocorrência.
+Deverá ser implementado um método para verificar a validade de cada tipo de triângulo.
 Estes métodos deverão retornar um valor lógico (true ou false)."""
 
 def Validar(A, B, C):
