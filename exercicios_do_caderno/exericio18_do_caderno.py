77094cd3af0b4212f2f56404a52d65d4a5c19eb3
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-28 11:03:39 -0300

    Update exericio18_do_caderno.py

--- exercicios_do_caderno/exericio18_do_caderno.py
+++ exercicios_do_caderno/exericio18_do_caderno.py
@@ -1,4 +1,5 @@
-"""Elabore um programa que receba uma quantidade de numeros e mostre na tela a soma desses numeros. O usuario define a quantidade de numeros a ser digitado."""
+"""Elabore um programa que receba uma quantidade de numeros e mostre na tela a soma desses numeros.
+O usuario define a quantidade de numeros a ser digitado."""
 
 n = int(input("Quantos numeros deseja somar? "))
 soma = 0
