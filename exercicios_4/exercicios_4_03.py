1ba2b0b503570224d84e3d201f4fe66ed10a80c6
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-31 10:20:44 -0300

    Update exercicios_4_03.py

--- exercicios_4/exercicios_4_03.py
+++ exercicios_4/exercicios_4_03.py
@@ -1,6 +1,5 @@
-"""Escreva um programa que leia uma matriz (3x5 ou 5x3) de 15 números
-inteiros e exiba ao final a soma dos valores de cada linha que
-estão armazenados nesta matriz."""
+"""Escreva um programa que leia uma matriz (3x5 ou 5x3) de 15 números inteiros e exiba ao final
+a soma dos valores de cada linha que estão armazenados nesta matriz."""
 
 matriz = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 valores = 0
