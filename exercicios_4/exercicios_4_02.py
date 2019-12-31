82e93e558b6863688370830d49c5fecdd1e60bb6
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-31 10:20:20 -0300

    Update exercicios_4_02.py

--- exercicios_4/exercicios_4_02.py
+++ exercicios_4/exercicios_4_02.py
@@ -1,7 +1,8 @@
-"""Escreva um programa que leia o nome e o preço de 10 produtos.
-Logo após realizar o cadastro dos produtos, pedir para o usuário digitar
-um valor, no qual deverá ser realizada uma pesquisa e exibir apenas
-os produtos que possuem preço até o valor digitado pelo usuário."""
+"""
+Escreva um programa que leia o nome e o preço de 10 produtos.
+Logo após realizar o cadastro dos produtos, pedir para o usuário digitar um valor,
+no qual deverá ser realizada uma pesquisa e exibir apenas os produtos que possuem preço até o valor digitado pelo usuário.
+"""
 
 nomes = []
 precos = []
