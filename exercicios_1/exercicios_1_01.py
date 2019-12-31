df70e2bd23686496ac28ca8ea1db90763c56f0d4
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-31 10:16:36 -0300

    Update exercicios_1_01.py

--- exercicios_1/exercicios_1_01.py
+++ exercicios_1/exercicios_1_01.py
@@ -1,5 +1,5 @@
-# Escreva um programa que armazene em variáveis o seu nome, sua idade e a cidade onde reside.
-# Na sequencia exiba o conteúdo destas variáveis na tela.
+"""Escreva um programa que armazene em variáveis o seu nome, sua idade e a cidade onde reside.
+Na sequencia exiba o conteúdo destas variáveis na tela."""
 
 nome = "Ana"
 idade = 17
