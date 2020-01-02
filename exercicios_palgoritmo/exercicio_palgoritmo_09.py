3894162d87d31657852fa51a697884bc88855240
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2020-01-02 10:35:29 -0300

    Update exercicio_palgoritmo_09.py

--- exercicios_palgoritmo/exercicio_palgoritmo_09.py
+++ exercicios_palgoritmo/exercicio_palgoritmo_09.py
@@ -1,5 +1,7 @@
 """Elaborar um algoritmo que efetue a leitura de um valor que esteja entre a faixa de 1 a 9.
-Após a leitura do valor fornecido pelo usuário, o programa deverá indicar uma de duas mensagens: “O valor está na faixa permitida”, caso o usuário forneça o valor nesta faixa, ou a mensagem “O valor está fora da faixa permitida”, caso o usuário forneça valores menores que 1 ou maiores que 9."""
+Após a leitura do valor fornecido pelo usuário, o programa deverá indicar uma de duas mensagens:
+“O valor está na faixa permitida”, caso o usuário forneça o valor nesta faixa,
+ou a mensagem “O valor está fora da faixa permitida”, caso o usuário forneça valores menores que 1 ou maiores que 9."""
 
 num = int(input("Digite um numero: "))
 if num >= 1 and num <= 9:
