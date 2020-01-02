0877d5319e6b9193079915b3710de684f5716496
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2020-01-02 10:35:05 -0300

    Update exercicio_palgoritmo_08.py

--- exercicios_palgoritmo/exercicio_palgoritmo_08.py
+++ exercicios_palgoritmo/exercicio_palgoritmo_08.py
@@ -1,5 +1,7 @@
-"""Supondo que a população de um país A seja da ordem de 90.000.000 habitantes com uma taxa anual de crescimento de 3% e que a população de um país B seja aproximadamente de 200.000.000 habitantes com uma taxa anual de crescimento de 1,5%.
-Fazer um algoritmo que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas essas taxas de crescimento."""
+"""Supondo que a população de um país A seja da ordem de 90.000.000 habitantes com uma taxa anual de crescimento de 3%
+e que a população de um país B seja aproximadamente de 200.000.000 habitantes com uma taxa anual de crescimento de 1,5%.
+Fazer um algoritmo que calcule e escreva o número de anos necessários para que a população do país A
+ultrapasse ou iguale a população do país B, mantidas essas taxas de crescimento."""
 
 a = 90000000
 b = 200000000
